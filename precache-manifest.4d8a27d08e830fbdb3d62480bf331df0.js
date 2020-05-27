0dd5c0c99942410e9de61139d0f0301d0eedf419
Ibtid <ibtid011@example.com>
2020-05-27 20:15:03 +0600

    Updates

---
+++ precache-manifest.4d8a27d08e830fbdb3d62480bf331df0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465ff8e2b976b6a528ddaa86ee661acb",
+    "url": "/reactToDoList/index.html"
+  },
+  {
+    "revision": "422d1ee7f6a8cfdb1c03",
+    "url": "/reactToDoList/static/css/main.cc31fd63.chunk.css"
+  },
+  {
+    "revision": "b3d4ff473baf9b38c5f4",
+    "url": "/reactToDoList/static/js/2.a36371f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactToDoList/static/js/2.a36371f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422d1ee7f6a8cfdb1c03",
+    "url": "/reactToDoList/static/js/main.453f323b.chunk.js"
+  },
+  {
+    "revision": "da9728e614f6f2b3bf2e",
+    "url": "/reactToDoList/static/js/runtime-main.3b16aa8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
